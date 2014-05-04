36f6afb2764c002c95affbd1f4f5464a7cd218f5
Brandon Brotsky <brandon@brotskydesigns.com>
2014-05-04 13:44:56 -0700

    fixed JS for scrolling boxes slider

--- wp-content/themes/wpthemeframework/includes/modules/scrolling-boxes-slider/scrolling-boxes-slider.js
+++ wp-content/themes/wpthemeframework/includes/modules/scrolling-boxes-slider/scrolling-boxes-slider.js
@@ -4,29 +4,25 @@ var img2,first2,row2;
 
 function imgScroll1(){
     row1 = $("#scrolling-boxes-slider .image-row.one");
-    first1 = $("#scrolling-boxes-slider .image-row.one .image").first();
-    img1 = $("#scrolling-boxes-slider .image-row.one .image");
-    
+    last1 = $("#scrolling-boxes-slider .image-row.one .image").last();
+    row1.css("left",0);
     row1.animate({left:"+=310"},12000,'linear',function(){
         
         row1.css("left",0);
         
-        first1.remove();
-        $("#scrolling-boxes-slider .image-row.one").append(first1);
+        last1.remove();
+        $("#scrolling-boxes-slider .image-row.one").prepend(last1);
         imgScroll1();
     });
 }
 
 function imgScroll2(){
     row2 = $("#scrolling-boxes-slider .image-row.two");
-    last2 = $("#scrolling-boxes-slider .image-row.two .image").last();
-    img2 = $("#scrolling-boxes-slider .image-row.two .image");
-    
+    last2 = $("#scrolling-boxes-slider .image-row.two .image").last();    
+    row2.css("left",-155);
     row2.animate({left:"+=310"},12000,'linear',function(){
         
-        row2.css({
-            "left":0
-        });
+        row2.css("left",-155);
         
         last2.remove();
         $("#scrolling-boxes-slider .image-row.two").prepend(last2);
