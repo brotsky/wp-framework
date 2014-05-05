6118daee81df72df52c7f4b09920187f44f78729
Brandon Brotsky <brandon@brotskydesigns.com>
2014-05-04 17:37:07 -0700

    changed the direction of the slider

--- wp-content/themes/wpthemeframework/includes/modules/scrolling-boxes-slider/scrolling-boxes-slider.js
+++ wp-content/themes/wpthemeframework/includes/modules/scrolling-boxes-slider/scrolling-boxes-slider.js
@@ -5,8 +5,8 @@ var img2,first2,row2;
 function imgScroll1(){
     row1 = $("#scrolling-boxes-slider .image-row.one");
     last1 = $("#scrolling-boxes-slider .image-row.one .image").last();
-    row1.css("left",0);
-    row1.animate({left:"+=310"},24000,'linear',function(){
+    row1.css("left",290);
+    row1.animate({left:"-=310"},24000,'linear',function(){
         
         row1.css("left",0);
         
@@ -19,10 +19,10 @@ function imgScroll1(){
 function imgScroll2(){
     row2 = $("#scrolling-boxes-slider .image-row.two");
     last2 = $("#scrolling-boxes-slider .image-row.two .image").last();    
-    row2.css("left",-155);
-    row2.animate({left:"+=310"},24000,'linear',function(){
+    row2.css("left",145);
+    row2.animate({left:"-=310"},24000,'linear',function(){
         
-        row2.css("left",-155);
+        row2.css("left",155);
         
         last2.remove();
         $("#scrolling-boxes-slider .image-row.two").prepend(last2);
