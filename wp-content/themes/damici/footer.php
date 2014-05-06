04f9cddb358ed778023595a9f74ff4a1e9bf20ff
Brandon Brotsky <brandon@brotskydesigns.com>
2014-05-05 18:35:58 -0700

    splash page and some styling fixes

--- wp-content/themes/damici/footer.php
+++ wp-content/themes/damici/footer.php
@@ -1,17 +1,18 @@
-	    <?php if(!is_home()) { ?>
-	    </div>
-	    <?php } ?>
+
 	<footer>
+	    <?php if(!is_page_template('splash-template.php')) { ?>
 	    <img class="diagonal-red" src="<?php echo get_stylesheet_directory_uri() ?>/images/diagonal-red.png" alt="" />
-	    
+	    <?php } ?>
 	    <div class="container">
+	        <?php if(!is_page_template('splash-template.php')) { ?>
 	        <div class="row">
 	            <div class="col-md-6 col-md-offset-3 text-center">
 	                <img id="footer-pig" src="<?php echo get_stylesheet_directory_uri() ?>/images/pig.png" alt="Pig" />
 	            </div>
 	        </div>
+            <?php } ?>
 	        <div class="row">
-	            <div class="col-md-6">
+	            <div class="col-md-6 text-left">
     	            <p>Copyright &#169; d'Amici. <?php echo date("Y"); ?></p>
     	            <p>Website Developed by <a href="http://www.brotskydesigns.com/" target="_blank">Brotsky Designs</a></p>
 	            </div>
