1e1f1139abb7402a68a8da3a93536f47dd83a9b7
Noah J Anderson <noah.j.anderson@gmail.com>
2014-05-13 02:00:36 -0700

    noah

---
+++ wp-content/themes/wpthemeframework/includes/modules/twitter-feed-slider/twitter-feed-slider.js
@@ -0,0 +1,37 @@
+$ = jQuery;
+$(document).ready(function(){
+window.setInterval(next_slide,8000);
+});
+function next_slide(){
+	var current_slide = $('#twitter-feed-slider .current_slide');
+	console.log("current_slide " + current_slide);
+	var current_slide_number = parseInt(current_slide.data('slide-id'));
+	console.log('current_slide_number '+current_slide_number);
+	var slide_count = parseInt($('#twitter-feed-slider').data('slide-count'));
+	console.log('slide_count '+slide_count);
+	var new_slide_id ;
+	if ((current_slide_number + 1) === slide_count )
+	{
+		new_slide_id = 0;
+	}
+	else
+	{
+		new_slide_id = current_slide_number + 1;
+	}
+	console.log('new_slide_id ' + new_slide_id);
+	var new_slide = $('#twitter-feed-slider [data-slide-id='+new_slide_id+']');
+	console.log('new_slide_id '+new_slide);
+	current_slide.fadeOut("slow",function()
+	{
+		current_slide.removeClass("current_slide");
+		current_slide.hide();
+		new_slide.fadeIn("slow",function()
+		{
+			new_slide.addClass("current_slide");
+			new_slide.removeClass("off_screen");
+		});
+	});
+	// new_slide.removeClass('off_screen').addClass('current_slide');
+	
+
+}
--- wp-content/themes/wpthemeframework/includes/modules/twitter-feed-slider/twitter-feed-slider.php
+++ wp-content/themes/wpthemeframework/includes/modules/twitter-feed-slider/twitter-feed-slider.php
@@ -8,6 +8,100 @@ define('TWITTER_CONSUMER_KEY', get_field("twitter_api_key","options"));
 define('TWITTER_CONSUMER_SECRET', get_field("twitter_api_secret","options"));
 define('TWITTER_OAUTH_CALLBACK', TWITTER_FEED_SLIDER_DIR . '/twitteroauth/callback.php');
 
+function buildBaseString($baseURI, $method, $params) {
+    $r = array();
+    ksort($params);
+    foreach($params as $key=>$value){
+        $r[] = "$key=" . rawurlencode($value);
+    }
+    return $method."&" . rawurlencode($baseURI) . '&' . rawurlencode(implode('&', $r));
+}
+
+function buildAuthorizationHeader($oauth) {
+    $r = 'Authorization: OAuth ';
+    $values = array();
+    foreach($oauth as $key=>$value)
+        $values[] = "$key=\"" . rawurlencode($value) . "\"";
+    $r .= implode(', ', $values);
+    return $r;
+}
+
+function return_tweet(){
+    $oauth_access_token         = "1892688138-yeZZTVLE50Xn6JLQzmQ97nIq8J8tVqMlwWnWygt";
+    $oauth_access_token_secret  = "QDKRL5qC3w4Omda96PHOmfRCyAM1d4FUdQyKsHUCxFVGK";
+    $consumer_key               = "PzuVmIDEOaTuZRQJipW7jikzo";
+    $consumer_secret            = "p9MkJdQ4wSP6SckogZBXP5eCU5ck1M9HIK2TSrKycMZBWgDn5p";
+
+    $twitter_timeline           = "user_timeline";  //  mentions_timeline / user_timeline / home_timeline / retweets_of_me
+
+    //  create request
+        $request = array(
+            'screen_name'       => 'CrunchPunchShop',
+            'count'             => '200'
+        );
+
+    $oauth = array(
+        'oauth_consumer_key'        => $consumer_key,
+        'oauth_nonce'               => time(),
+        'oauth_signature_method'    => 'HMAC-SHA1',
+        'oauth_token'               => $oauth_access_token,
+        'oauth_timestamp'           => time(),
+        'oauth_version'             => '1.0'
+    );
+
+    //  merge request and oauth to one array
+        $oauth = array_merge($oauth, $request);
+
+    //  do some magic
+        $base_info              = buildBaseString("https://api.twitter.com/1.1/statuses/$twitter_timeline.json", 'GET', $oauth);
+        $composite_key          = rawurlencode($consumer_secret) . '&' . rawurlencode($oauth_access_token_secret);
+        $oauth_signature            = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true));
+        $oauth['oauth_signature']   = $oauth_signature;
+
+    //  make request
+        $header = array(buildAuthorizationHeader($oauth), 'Expect:');
+        $options = array( CURLOPT_HTTPHEADER => $header,
+                          CURLOPT_HEADER => false,
+                          CURLOPT_URL => "https://api.twitter.com/1.1/statuses/$twitter_timeline.json?". http_build_query($request),
+                          CURLOPT_RETURNTRANSFER => true,
+                          CURLOPT_SSL_VERIFYPEER => false);
+
+        $feed = curl_init();
+        curl_setopt_array($feed, $options);
+        $json = curl_exec($feed);
+        curl_close($feed);
+
+    return json_decode($json, true);
+}
+
+
+
+
+
+
+
+// function twitter_oauth2(){
+// 	$key=rawurlencode('PzuVmIDEOaTuZRQJipW7jikzo');
+// 	$secret = rawurlencode('p9MkJdQ4wSP6SckogZBXP5eCU5ck1M9HIK2TSrKycMZBWgDn5p');
+// 	$url = $key.':'.$secret;
+// 	$encode_url = base64_encode ($url);
+// 	$url = 'https://api.twitter.com/oauth2/token';
+// 	$data = array('grant_type' => 'client_credentials');
+
+// 	// use key 'http' even if you send the request to https://...
+// 	$options = array(
+// 	    'http' => array(
+// 			"header" => ["Content-type: application/x-www-form-urlencoded;charset=UTF-8",
+// 						"Authorization: Basic".$encode_url ],
+// 			'method'  => 'POST',
+// 	        'content' => http_build_query($data)
+//     )
+// 	);
+// $context  = stream_context_create($options);
+// $result = file_get_contents($url, false, $context);
+// var_dump($result);
+// }
+
 //$screen_name = get_field("twitter_feed_slider_screen_name","options");
 //$count = get_field("twitter_feed_count","options");
 
@@ -29,30 +123,37 @@ $content = $connection->get("statuses/home_timeline");
 
 function twitter_feed_slider() {
     twitter_feed_slider_scripts();
-    
+    $twitter_name="CrunchPunchShop";
+    $twitter_feed = return_tweet();
+    // var_dump($twitter_feed);
     ?>
-    <section id="twitter-feed-slider">
-            <div class="row feed_wrapper">
-                <div class="col-md-12 text-center " >
+
+    <section id="twitter-feed-slider" data-slide-count='<?=count($twitter_feed)?>' >
+            <div class="row feed_wrapper" >
+                <div class="col-md-12 text-center" >
                 <span class='fa fa-twitter fa-5x tweet_icon'></span>
                 </div>
-                <div class="col-md-12 text-center" >
-                #CrunchPunch
+                <div class="col-md-12 text-center tweet_name" >
+                #CrunchPunchShop
                 </div>
-                <div class="container feed_inner" >
+                <? foreach ($twitter_feed as $key => $value) {?>
+                <div class="container feed_inner <? if ($key==0) echo 'current_slide'; else echo 'off_screen';?>" data-slide-id='<?=$key?>'>
                 <div class='col-md-1 feed_quote'>&ldquo;
                 </div>
                 <div class='col-md-8'>
-                Blah Blah Blah BlahBlah Blah Blah BlahBlah Blah Blah BlahBlah Blah Blah BlahBlah Blah Blah BlahBlah Blah Blah BlahBlah Blah Blah BlahBlah Blah Blah BlahBlah Blah Blah BlahBlah Blah Blah BlahBlah Blah Blah BlahBlah Blah Blah BlahBlah Blah Blah BlahBlah Blah Blah BlahBlah Blah Blah BlahBlah Blah Blah BlahBlah Blah Blah BlahBlah Blah Blah BlahBlah Blah Blah BlahBlah Blah Blah BlahBlah Blah Blah Blah
+                <?=$value['text']?>
                 </div>
                 <div class='col-md-3'>
                 <div class="tweet_bubble">
-                hello world
+                <?=$value['user']['name']?>
+				<?=$value['user']['screen_name']?>
                 </div>
                 </div>
-                </div>
-            </div>
+            	</div>
+            	<?}?>
+           	</div>
     </section>
+
     <?php
 }
 
