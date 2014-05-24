605d06eaa216d561b45aae2591eff5768c171b22
Noah J Anderson <noah.j.anderson@gmail.com>
2014-05-23 20:39:01 -0700

    Noah

--- wp-content/themes/wpthemeframework/includes/modules/google-map-contact/google-map-contact.php
+++ wp-content/themes/wpthemeframework/includes/modules/google-map-contact/google-map-contact.php
@@ -4,30 +4,34 @@ define('GOOGLE_MAP_CONTACT_DIR', get_template_directory_uri() . '/includes/modul
 
 function google_map_contact() {
 	google_map_contact_scripts();
-		if (get_field('company_address'))
+	if ( get_field('google_embed_key') )
+	{
+		if ( get_field('company_name'))
 		{
-			if ( !get_field('latitude') && !get_field('longitude') )
-			{//if latitude and longitude are avaiable create map?>
-				<section id="google-map-contact">
-				<div class="row">
-				<div class="col-md-12 wrapper">
-					<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
-					  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDUb-AV0h3Lss_Y27ZvVHCGZSlV3nkUfqI&q=Damici,Calistoga+CA&zoom=18">
-					</iframe>
-				</div>
-				</div>
-				</section>
+			if ( get_field('city') && get_field('state') )
+			{//if latitude and longitude are avaiable create map
+				$google_map_url = 'https://www.google.com/maps/embed/v1/place?key='.get_field('google_embed_key').'&q='.get_field('company_name').','.get_field('city').'+'.get_field('state').'&zoom=18';?>
 			<?}
 			else
 			{//else Geocode address and save lat and longitude?>
-				<h1>No Lat and Long Encode it</h1>
+				<? $google_map_url = 'https://www.google.com/maps/embed/v1/place?key='.get_field('google_embed_key').'&q='.get_field('company_name').'&zoom=18'; ?>
 			<?}?>
+			<section id="google-map-contact">
+				<div class="row">
+				<div class="col-md-12 wrapper">
+					<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+					  src='<?echo $google_map_url;?>'
+					</iframe>
+				</div>
+				</div>
+			</section>
 			<!-- <h1>Hello Company Address</h1> -->
 		<?}
 		else
 		{//neither address nor latitude and longitude are available so don't show map?>
 			<h1>No Company Address == No Map</h1>
 		<?}
+	}
 }
 function google_map_contact_scripts() {
     wp_enqueue_style( 'google-map-contact', GOOGLE_MAP_CONTACT_DIR . '/google-map-contact.css' );
@@ -36,70 +40,82 @@ function google_map_contact_scripts() {
 
 if(function_exists("register_field_group"))
 {
-register_field_group(array (
-	'id' => 'acf_google_map_contact',
-	'title' => 'google_map_contact',
-	'fields' => array (
-		array (
-			'key' => 'field_537f7b387570d',
-			'label' => 'Company Address',
-			'name' => 'company_address',
-			'type' => 'text',
-			'instructions' => 'Enter an address for Geocoding into Latitude and Longitude Coordinates which are required to create a Pushpin on a Google Map.',
-			'default_value' => '',
-			'placeholder' => 'Address for Geocoding',
-			'prepend' => '',
-			'append' => '',
-			'formatting' => 'html',
-			'maxlength' => '',
-		),
-		array (
-			'key' => 'field_537f7c827570e',
-			'label' => 'Latitude',
-			'name' => 'latitude',
-			'type' => 'text',
-			'instructions' => 'Auto filled or can manually input a Latitude from Geocoded Address.',
-			'default_value' => '',
-			'placeholder' => 'Latitude',
-			'prepend' => '',
-			'append' => '',
-			'formatting' => 'html',
-			'maxlength' => '',
-		),
-		array (
-			'key' => 'field_537f7ce57570f',
-			'label' => 'Longitude',
-			'name' => 'longitude',
-			'type' => 'text',
-			'instructions' => 'Auto filled or can manually input a Longitude from Geocoded Address.',
-			'default_value' => '',
-			'placeholder' => '',
-			'prepend' => '',
-			'append' => '',
-			'formatting' => 'html',
-			'maxlength' => '',
-		),
-	),
-	'location' => array (
-		array (
+	register_field_group(array (
+		'id' => 'acf_google-map-contact',
+		'title' => 'google-map-contact',
+		'fields' => array (
 			array (
-				'param' => 'post_type',
-				'operator' => '==',
-				'value' => 'post',
-				'order_no' => 0,
-				'group_no' => 0,
+				'key' => 'field_5380107130104',
+				'label' => 'Company Name',
+				'name' => 'company_name',
+				'type' => 'text',
+				'instructions' => 'Add the company Name for a google place search',
+				'default_value' => '',
+				'placeholder' => 'Company Name',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+			array (
+				'key' => 'field_538010a230105',
+				'label' => 'City',
+				'name' => 'city',
+				'type' => 'text',
+				'default_value' => '',
+				'placeholder' => 'City',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+			array (
+				'key' => 'field_538010bc30106',
+				'label' => 'State',
+				'name' => 'state',
+				'type' => 'text',
+				'instructions' => 'Company State',
+				'default_value' => '',
+				'placeholder' => 'State',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+			array (
+				'key' => 'field_5380114114e95',
+				'label' => 'Google Embed Key',
+				'name' => 'google',
+				'type' => 'text',
+				'default_value' => '',
+				'placeholder' => 'Google Embed Key',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
 			),
 		),
-	),
-	'options' => array (
-		'position' => 'normal',
-		'layout' => 'no_box',
-		'hide_on_screen' => array (
+		'location' => array (
+			array (
+				array (
+					'param' => 'options_page',
+					'operator' => '==',
+					'value' => 'acf-options-theme-options',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
 		),
-	),
-	'menu_order' => 0,
-));
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
 }
+
 
 function google_map_contact_shortcode( $atts, $content = null ) {
     return google_map_contact();
